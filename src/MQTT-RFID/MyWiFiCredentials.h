369bd68d455bbe8eb877dc814ee2da2c05ef2309
JerryG <42826502+jerryg2003@users.noreply.github.com>
2023-05-17 15:32:58 -0400

    Create MyWiFiCredentials.h

---
+++ src/MQTT-RFID/MyWiFiCredentials.h
@@ -0,0 +1,5 @@
+//WiF and JMRI Credentials
+const char* ssid         = "ssid";
+const char* password     = "pword";
+const int   jmriRfidPort = 12099;                              //Make sure JMRI RFID connection (if used) set to this port
+const char* clientName   = "ESP32-XXX";            // known to wireless network
